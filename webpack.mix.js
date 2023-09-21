9264fdb92ef926d37a03758a20b6b9a1859baa6c
Maksym Bratuta <m.bratuta@gmail.com>
2023-09-21 09:50:58 +0300

    Set up Breeze & Inertia

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,12 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
